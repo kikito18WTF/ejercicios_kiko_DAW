7912e3b9575ed238d8119331e8ba37c61d31914e
kikito18WTF <154981081+kikito18WTF@users.noreply.github.com>
2024-12-11 20:37:00 +0100

    Update Ejercicios.java

--- ejercicios/src/main/java/org/example/Ejercicios.java
+++ ejercicios/src/main/java/org/example/Ejercicios.java
@@ -2298,7 +2298,112 @@ public class Ejercicios {
 
         System.out.println("Palabra no encontrada.");
     }
+ public void bingo(){
 
+        Random aleatorio = new Random();
+        Scanner teclado = new Scanner(System.in);
+
+        System.out.println("*** BIENVENIDO AL BINGO DEL CASINO CANTÁBRICO ***");
+
+        //a)
+        int bolas = aleatorio.nextInt(31)+10;
+        Integer vector[] = new Integer[bolas];
+        int num_aleatorio = 0;
+
+        for (int i = 0; i < vector.length; i++) {
+
+            do{
+                num_aleatorio = aleatorio.nextInt(90)+1;
+            }while(Arrays.asList(vector).contains(num_aleatorio));  //a) y b) crear vector comprobando repetidos
+
+            vector[i] = num_aleatorio;
+
+        }
+
+        System.out.print("\n");
+
+        System.out.println(bolas + " bolas extraídas hasta ahora: " + Arrays.toString(vector));
+
+        System.out.print("\n");
+
+        //c)
+        System.out.println("*** Introduce los datos de tu cartón ***");
+
+        int[][] carton = new int[3][3];
+
+        for (int i = 0; i < carton.length; i++) {
+
+            System.out.println("Fila " + (i+1) + ": ");
+            String valores = teclado.next();
+
+            if (!valores.matches("\\d{1,2}-\\d{1,2}-\\d{1,2}")){
+                System.out.println("Cerrando programa... Introduce valores con el formato correcto (N-N-N).");
+                return;
+            }
+
+            String fila[] = valores.split("-");
+
+            for (int j = 0; j < carton[0].length; j++) {
+                carton[i][j] = Integer.parseInt(fila[j]);
+            }
+
+        }
+
+        //d)
+        System.out.println("Datos del cartón introducido: ");
+        for(int[] filas : carton){
+            for(int columnas : filas){
+                System.out.print(columnas + " ");
+            }
+            System.out.print("\n");
+        }
+
+        System.out.print("\n");
+
+
+        System.out.println("PREMIOS: ");
+
+        System.out.print("\n");
+
+        //e)
+        //compruebo bingo
+        boolean haybingo = true;
+        bingo:
+        for (int i = 0; i < carton.length; i++) {
+            for (int j = 0; j < carton[0].length; j++) {
+
+                if(!Arrays.asList(vector).contains(carton[i][j])){
+                    System.out.println("No hay BINGO.");
+                    haybingo = false;
+                    break bingo;
+                }
+            }
+        }
+
+        if (haybingo){
+            System.out.println("HAY BINGO!!");
+            return;
+        }
+
+        //f)
+        //si no hay BINGO compruebo línea
+        linea:
+        for (int i = 0; i < carton.length; i++) {
+            for (int j = 0; j < carton[0].length; j++) {
+
+                if(!Arrays.asList(vector).contains(carton[i][j])){
+                    System.out.println("Línea " + (i+1) + ": NO");
+                    continue linea;
+                }
+
+            }
+
+            System.out.println("Línea " + (i+1) + ": CORRECTA!!");
+            break;
+
+        }
+
+    }
 }
 
 
