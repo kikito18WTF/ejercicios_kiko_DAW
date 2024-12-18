e13db83a89df7438b61488f160562f03e55472c0
kikito18WTF <154981081+kikito18WTF@users.noreply.github.com>
2024-12-18 02:05:18 +0100

    Update Nochevieja.java

--- ejercicios/src/main/java/org/example/Nochevieja.java
+++ ejercicios/src/main/java/org/example/Nochevieja.java
@@ -4,15 +4,16 @@ import java.util.Scanner;
 /**
  * @author: Kiko Perez
  * @version: 1.0 (15/12/2024)
+ * @deprecated Método principal que ejecuta el programa lee las horas y calcula los minutos restantes hasta medianoche para cada hora introducida
  */
 
 public class Nochevieja {
 
     /**
      * Método principal que ejecuta el programa lee las horas y calcula los minutos restantes hasta medianoche para cada hora introducida.
-     * El programa termina cuando se ingresa la hora "00:00".
+     * El programa termina cuando se ingresa la hora "00:00"
      *
-     * @param args - Los argumentos de la línea de comandos (no utilizados en este caso).
+     * @param args - Los argumentos de la línea de comandos
      */
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
@@ -20,14 +21,14 @@ public class Nochevieja {
         while (true) {
             String hora = entrada.nextLine();
 
-            if (MEDIANOCHE(hora)) {
+            if (medianoche(hora)) {
                 break;
             }
 
             int minutosquequedan = calcularMinutos(hora);
 
             if (minutosquequedan != -1) {
-                System.out.println(minutosquequedan);
+                System.out.println(minutosquequedan+ " minutos");
             } else {
                 System.out.println("Formato de hora no válido");
             }
@@ -37,22 +38,22 @@ public class Nochevieja {
     }
 
     /**
-     * Verifica si la hora ingresada es medianoche .
+     * Verifica si la hora ingresada es medianoche
      *
-     * @param hora - La hora a verificar en formato "HH:mm".
-     * @return true si la hora es 00:00, de lo contrario, false.
+     * @param hora - La hora a verificar en formato "HH:mm"
+     * @return true si la hora es 00:00, de lo contrario, false
      */
 
-    public static boolean MEDIANOCHE (String hora) {
+    public static boolean medianoche (String hora) {
         return "00:00".equals(hora);
     }
 
     /**
-     * Calcula los minutos restantes hasta medianoche, dado un valor de hora en formato HH:mm.
-     * Si la hora o los minutos no son válidos, devuelve -1.
+     * Calcula los minutos restantes hasta medianoche, dado un valor de hora en formato hh:mm
+     * Si la hora o los minutos no son válidos, devuelve -1
      *
-     * @param hora - La hora en formato HH:mm que se desea calcular.
-     * @return Los minutos restantes hasta medianoche, o -1 si la hora no es válida.
+     * @param hora - La hora en formato hh:mm que se desea calcular
+     * @return Los minutos restantes hasta medianoche, o -1 si la hora no es válida
      */
 
     public static int calcularMinutos(String hora) {
@@ -77,3 +78,4 @@ public class Nochevieja {
         }
     }
 }
+
