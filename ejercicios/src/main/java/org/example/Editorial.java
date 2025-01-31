fc3ce3754f483eeb83d03822ff338b488041716e
kikito18WTF <154981081+kikito18WTF@users.noreply.github.com>
2025-01-31 14:45:41 +0100

    Create Editorial.java

---
+++ ejercicios/src/main/java/org/example/Editorial.java
@@ -0,0 +1,55 @@
+package org.example;
+
+import java.util.ArrayList;
+
+public class Editorial {
+    private String nombre;
+    private String pais;
+
+
+
+
+    private ArrayList<Libro>librosPublicados;
+
+    public Editorial(String nombre, String pais){
+        this.nombre=nombre;
+        this.pais=pais;
+
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getPais() {
+        return pais;
+    }
+
+    public void setPais(String pais) {
+        this.pais = pais;
+    }
+    public ArrayList<Libro> getLibrosPublicados() {
+        return librosPublicados;
+    }
+
+    public void setLibrosPublicados(ArrayList<Libro> librosPublicados) {
+        this.librosPublicados = librosPublicados;
+    }
+
+    public void anyadirlibro(Libro libro){
+        librosPublicados.add(libro);
+    }
+
+    public void borrarLibro(Libro libro){
+        librosPublicados.remove(libro);
+    }
+
+    @Override
+    public String toString(){
+        return " Editorial :  [ nombre=" + getNombre() +  " pais=" + getPais() + " ]";
+    }
+}
